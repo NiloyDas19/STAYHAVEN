2d7d09c7a71873db54e672b9b00499b6c86fed02
Niloy Das <niloydas5215@gmail.com>
2024-04-10 09:55:09 +0600

    update profile done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 
-const Home = () => {
+const Home = () => {    
+
     return (
         <div>
             <h2 className="text-3xl">This is Home Section</h2>
--- src/components/UpdateProfile/UpdateProfile.jsx
+++ src/components/UpdateProfile/UpdateProfile.jsx
@@ -1,8 +1,68 @@
+import {updateProfile } from "firebase/auth";
+import auth from "../../firebase/firebase.config";
+import swal from 'sweetalert';
+import { useNavigate } from 'react-router-dom';
+
+
 
 const UpdateProfile = () => {
+    const navigate = useNavigate();
+
+    const handleUpdateProfile = (e) => {
+        e.preventDefault();
+        updateProfile(auth.currentUser, {
+            displayName: e.target.name.value, photoURL: e.target.photoUrl.value
+        }).then(() => {
+            console.log("what happens !");
+            swal({
+                title: "Good job!",
+                text: "Profile Updated Successfully!",
+                icon: "success",
+            })
+            .then(ok => {
+                if(ok)  {
+                    window.location.reload();
+                }
+            })
+            navigate("/");
+        }).catch((error) => {
+            // An error occurred
+            // ...
+            swal({
+                icon: "error",
+                title: "Oops...",
+                text: error.message,
+            });
+            console.log(error);
+        });
+        e.target.reset();
+    }
+
+
     return (
-        <div>
-            <h2 className="text-3xl">This is Update Profile Page.</h2>
+        <div className="hero min-h-screen bg-base-200 mx-auto">
+            <div className="card w-full md:w-1/2 max-w-sm md:max-w-xl shadow-2xl bg-base-100 p-10">
+                <div className="text-center mt-5">
+                    <h2 className="text-2xl md:text-4xl text-blue-500 font-bold">Update Profile</h2>
+                </div>
+                <form className="w-full" onSubmit={handleUpdateProfile}>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" placeholder="name" name="name" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Photo URL</span>
+                        </label>
+                        <input type="text" placeholder="Photo URL" name="photoUrl" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Save Changes</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
